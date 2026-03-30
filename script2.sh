febc151162ccc74a56535a9b43072b2fe0a08df4
Pranjul-01 <pranjul.24bac10063@vitbhopal.ac.in>
2026-03-30 11:44:44 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,1 +1,20 @@
 script2.sh
+#!/bin/bash
+# Script 2: FOSS Package Inspector
+
+PACKAGE="git"
+
+if dpkg -l | grep -q $PACKAGE; then
+    echo "$PACKAGE is installed."
+    dpkg -s $PACKAGE | grep -E 'Version|Maintainer|Description'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+case $PACKAGE in
+    git) echo "Git: distributed version control system for developers";;
+    apache2) echo "Apache: open-source web server powering websites";;
+    mysql) echo "MySQL: relational database system";;
+    firefox) echo "Firefox: open-source web browser";;
+    *) echo "Unknown package";;
+esac
